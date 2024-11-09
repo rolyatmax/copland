4ba3e59829a14282a204b0ee13078dc770740cd3
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-11-09 17:56:57 -0500

    fix top-level async

--- src/main.tsx
+++ src/main.tsx
@@ -9,35 +9,39 @@ import './styles/normalize.css'
 import './styles/styles.css'
 import { checkWebGPU } from './lib/webgpu.ts'
 
-await checkWebGPU()
+async function main() {
+  await checkWebGPU()
 
-// const canPlayMP3 = new Audio().canPlayType('audio/mp3')
-// if (!canPlayMP3 || canPlayMP3 === 'maybe') {
-//   document.body.innerHTML = `
-//   <div style="font-size: 1.5em; margin: 100px auto; max-width: 800px; text-align: center;">
-//     This demo requires a browser that can play mp3s.
-//   </div>
-//   `
-//   throw new Error('MP3 not supported')
-// }
+  // const canPlayMP3 = new Audio().canPlayType('audio/mp3')
+  // if (!canPlayMP3 || canPlayMP3 === 'maybe') {
+  //   document.body.innerHTML = `
+  //   <div style="font-size: 1.5em; margin: 100px auto; max-width: 800px; text-align: center;">
+  //     This demo requires a browser that can play mp3s.
+  //   </div>
+  //   `
+  //   throw new Error('MP3 not supported')
+  // }
 
-const copland = new Copland(instrumentConfig)
-const renderer = await createRenderer(document.querySelector('canvas')!, copland)
+  const copland = new Copland(instrumentConfig)
+  const renderer = await createRenderer(document.querySelector('canvas')!, copland)
 
-const { hash } = document.location
-if (hash && hash.slice(0, 2) === '#/') {
-  copland.loadFromHash(hash.slice(2))
-} else {
-  copland.randomizePads()
+  const { hash } = document.location
+  if (hash && hash.slice(0, 2) === '#/') {
+    copland.loadFromHash(hash.slice(2))
+  } else {
+    copland.randomizePads()
+  }
+
+  copland.addOnReady(() => {
+    document.querySelector('#app')!.classList.add('show')
+    renderer.start()
+  })
+
+  const introRoot = createRoot(document.querySelector('#intro'))
+  introRoot.render(<Intro copland={copland} />)
+
+  const buttonsRoot = createRoot(document.querySelector('#btns'))
+  buttonsRoot.render(<Buttons copland={copland} />)
 }
 
-copland.addOnReady(() => {
-  document.querySelector('#app')!.classList.add('show')
-  renderer.start()
-})
-
-const introRoot = createRoot(document.querySelector('#intro'))
-introRoot.render(<Intro copland={copland} />)
-
-const buttonsRoot = createRoot(document.querySelector('#btns'))
-buttonsRoot.render(<Buttons copland={copland} />)
+main()
